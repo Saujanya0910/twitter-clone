329b8493bc8c482e67b8961c051f80c15cc19ddb
Saujanya Pandey <saujanya.pandey2000@gmail.com>
2021-07-12 00:06:21 +0530

    fixed feed template, likes model migrated

--- apps/feed/api.py
+++ apps/feed/api.py
@@ -11,3 +11,15 @@ def api_add_tweet(request):
   tweet = Tweet.objects.create(body=body, created_by=request.user)
 
   return JsonResponse({'success': True})
+
+
+@login_required
+def api_add_like(request):
+  data = json.loads(request.body)  # convert json to python dict
+  tweet_id = data['tweet_id']
+
+# if like is present and liked by the current user
+  if not Like.objects.filter(tweet_id=tweet_id).filter(created_by=request.user).exists():
+    like = Like.objects.filter(tweet_id=tweet_id)
+
+  return JsonResponse({'success': True})
